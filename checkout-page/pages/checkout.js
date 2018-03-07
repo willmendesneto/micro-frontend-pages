9c8a8b2ee8e3c72d9bb42a541ba8381f876a28dc
Will Mendes <willmendesneto@gmail.com>
2018-03-08 00:11:13 +1100

    feat(checkout-page): adding new checkout page

---
+++ checkout-page/pages/checkout.js
@@ -0,0 +1,7 @@
+import { CheckoutPage } from 'micro-frontend-react-pages';
+
+import 'micro-frontend-react-pages/src/scss/pages/checkout.scss';
+
+const Checkout = () => <CheckoutPage />
+
+export default Checkout;
